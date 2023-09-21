b4cae96f4fba78c576a1c81311ab65ad98f8bdf4
rookedsysc <67862775+rookedsysc@users.noreply.github.com>
2023-09-22 00:51:37 +0900

    config : Spring Initialize 없이 Custom하게 Spring Project 생성 #1 (#2)
    * feat : db api module 생성 및 maven repository 설정
    
    * config : Spring Boot Dependency 설정 및 Spring Boot Main
    
    * config : DB Module Dependency 변경
    
    * config : api, db module 각각 jar 파일 build 설정
    
    * feat : Entity, Repository 생성
    config : Repository 연결 및 module 의존성 설정
    
    * feat : Object Mapper Custom
    
    * feat : Swagger 연동 및 설정:

---
+++ delivery-java-project/db/src/main/java/org/delivery/db/BaseEntity.java
@@ -0,0 +1,19 @@
+package org.delivery.db;
+
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@Data
+@SuperBuilder
+@MappedSuperclass
+@NoArgsConstructor
+public class BaseEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+}
