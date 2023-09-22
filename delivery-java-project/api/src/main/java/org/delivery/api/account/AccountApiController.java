371bc70e0ad1d7725c343dc59f77955cdd97a983
rookedsysc <rookedsysc36@gmail.com>
2023-09-22 16:12:09 -0400

    feat : ApiException Handler (Runtime Exception 적용)

--- delivery-java-project/api/src/main/java/org/delivery/api/account/AccountApiController.java
+++ delivery-java-project/api/src/main/java/org/delivery/api/account/AccountApiController.java
@@ -3,7 +3,8 @@ package org.delivery.api.account;
 import lombok.RequiredArgsConstructor;
 import org.delivery.api.account.model.AccountMeResponse;
 import org.delivery.api.common.api.Api;
-import org.delivery.db.account.AccountEntity;
+import org.delivery.api.common.error.ErrorCode;
+import org.delivery.api.common.exception.ApiException;
 import org.delivery.db.account.AccountRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,8 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 
-@RestController
 @RequiredArgsConstructor
+@RestController
 @RequestMapping("/api/account")
 public class AccountApiController {
 
@@ -27,6 +28,12 @@ public class AccountApiController {
         .registeredAt(LocalDateTime.now())
         .build();
 
+    var str = "안녕하세요";
+    try {
+      var age = Integer.parseInt(str);
+    } catch (Exception e) {
+      throw new ApiException(ErrorCode.SERVER_ERROR, e, "숫자가 아닙니다.");
+    }
     return Api.OK(res);
   }
 }
---
+++ delivery-java-project/api/src/main/java/org/delivery/api/common/exception/ApiExceptioninterface.jav
@@ -0,0 +1,5 @@
+
+public interface ApiExceptionInterface {
+  ErrorCodeIfs getErrorCodeInterface();
+  String getErrorDescription();
+}
