28b5ecfddf4b616e233f64ebe737e2d8bc8a8daa
rookedsysc <rookedsysc36@gmail.com>
2023-10-22 02:44:30 +0900

    fix Interceptor 동작 안하던 버그 수정 #12 https://rookedsysc.vercel.app/java-and-spring/spring-boot/interceptor-and-filter\#resolver-null-point-exception

--- delivery-java-project/api/src/main/java/org/delivery/api/domain/token/web/WebConfig.kt
+++ delivery-java-project/api/src/main/java/org/delivery/api/domain/token/web/WebConfig.kt
@@ -4,13 +4,14 @@ import org.delivery.api.interceptor.AuthorizationInterceptor
 import org.delivery.api.resolver.UserSessionResolver
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 class WebConfig(
 	private val authorizationInterceptor: AuthorizationInterceptor,
 	private val userSessionResolver: UserSessionResolver
-): WebMvcConfigurer {
+) : WebMvcConfigurer {
 	private val OPEN_API = listOf(
 		"/open-api/**"
 	)
@@ -21,6 +22,14 @@ class WebConfig(
 	
 	private val SWAGGER =
 		listOf("/swagger-ui.html", "/swagger-ui/**", "/swagger-resources/**", "/v2/api-docs", "/v3/api-docs/**")
+	
+	override fun addInterceptors(registry: InterceptorRegistry) {
+		registry
+			.addInterceptor(authorizationInterceptor)
+			.excludePathPatterns(OPEN_API)
+			.excludePathPatterns(DEFAULT_EXCLUDE)
+			.excludePathPatterns(SWAGGER)
+	}
 	
 	override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
 		resolvers.add(userSessionResolver)
