d66c49fe03992578fa568fae7c6f211c3c1f9d01
rookedsysc <rookedsysc36@gmail.com>
2023-10-25 02:14:55 +0900

    feat Api Gateway Routing 설정 #13

---
+++ apigw/src/main/kotlin/com/example/apigw/filter/ServiceApiPrivateFilter.kt
@@ -0,0 +1,27 @@
+package com.example.apigw.filter
+
+import com.example.apigw.common.Log
+import org.springframework.cloud.gateway.filter.GatewayFilter
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory
+import org.springframework.stereotype.Component
+
+@Component
+class ServiceApiPrivateFilter: AbstractGatewayFilterFactory<ServiceApiPrivateFilter.config>(config::class.java) {
+	
+	companion object: Log
+	
+	class config
+	
+	override fun apply(config: config): GatewayFilter {
+		return GatewayFilter { exchange, chain ->
+			var uri = exchange.request.uri
+			
+			log.info("ServiceApiFilter uri: $uri")
+			
+			val mono = chain.filter(exchange)
+			
+			mono
+		}
+		
+	}
+}
--- apigw/src/test/kotlin/com/example/apigw/ApigwApplicationTests.kt
+++ apigw/src/test/kotlin/com/example/apigw/ApigwApplicationTests.kt
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class ApigwApplicationTests {
+class ApiGwApplicationTests {
 
 	@Test
 	fun contextLoads() {
