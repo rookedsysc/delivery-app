798802898e82b116e58894930f40c9dcaf761e07
rookedsysc <rookedsysc36@gmail.com>
2024-02-17 04:14:27 +0900

    modify Api GW 라우팅 수정

--- delivery-java-project/apigw/src/main/kotlin/org/delivery/apigw/route/RouteConfig.kt
+++ delivery-java-project/apigw/src/main/kotlin/org/delivery/apigw/route/RouteConfig.kt
@@ -1,6 +1,7 @@
 package org.delivery.apigw.route
 
 import org.delivery.apigw.filter.ServiceApiPrivateFilter
+import org.delivery.apigw.filter.ServiceApiPublicFilter
 import org.springframework.cloud.gateway.route.RouteLocator
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
 import org.springframework.context.annotation.Bean
@@ -8,27 +9,38 @@ import org.springframework.context.annotation.Configuration
 
 @Configuration
 class RouteConfig(
-	private val serviceApiPrivateFilter: ServiceApiPrivateFilter,
+        private val serviceApiPrivateFilter: ServiceApiPrivateFilter,
+        private val serviceApiPublicFilter: ServiceApiPublicFilter
 ) {
 
-	@Bean
-	fun gatewayRoutes(builder: RouteLocatorBuilder): RouteLocator {
-		return builder
-			.routes()
-			.route { spec ->
-				// 우선순위
-				spec.order(-1)
-				spec
-					.path("/service-api/api/**") // 매칭할 주소
-					.filters { filterSpec ->
-						filterSpec.filter(serviceApiPrivateFilter.apply(ServiceApiPrivateFilter.config())) // 필터 지정
-						filterSpec.rewritePath("/service-api(?<segment>/?.*)", "\${segment}")
-					}
-					.uri(
-						"http://localhost:8080" // 라우팅할 주소
-					)
-			}
-			.build()
-
-	}
+    @Bean
+    fun gatewayRoutes(builder: RouteLocatorBuilder): RouteLocator {
+        return builder
+            .routes()
+            .route { spec ->
+                // 우선순위
+                spec.order(-1)
+                spec
+                        .path("/service-api/api/**") // 매칭할 주소
+                        .filters { filterSpec ->
+                            filterSpec.filter(serviceApiPrivateFilter.apply(ServiceApiPrivateFilter.config())) // 필터 지정
+                            filterSpec.rewritePath("/service-api(?<segment>/?.*)", "\${segment}")
+                        }
+                        .uri(
+                                "http://localhost:8080" // 라우팅할 주소
+                        )
+            }
+            .route { spec ->
+                spec
+                        .path("/service-api/open-api/**") // 매칭할 주소
+                        .filters { filterSpec ->
+                            filterSpec.filter(serviceApiPublicFilter.apply(ServiceApiPublicFilter.config())) // 필터 지정
+                            filterSpec.rewritePath("/service-api(?<segment>/?.*)", "\${segment}")
+                        }
+                        .uri(
+                                "http://localhost:8080" // 라우팅할 주소
+                        )
+            }
+            .build()
+    }
 }
